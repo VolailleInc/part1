e01a2575a3b5cb4278079875cb6842bbbc52c2c5
Abdul-Razak <razaksaw@gmail.com>
2021-09-29 12:51:45 +0000

    Added comments and redifind functions

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -20,7 +20,7 @@ const Button = ({ onClick, text }) => {
 	);
 };
 
-//Reusable table
+//A component with table to display the value of each statistic
 const StatisticLine = ({ value, text }) => {
 	return (
 		<div>
@@ -36,6 +36,7 @@ const StatisticLine = ({ value, text }) => {
 	);
 };
 
+//A conditionally rendered no feedback component
 const NoFeedBack = () => {
 	return (
 		<div>
@@ -44,6 +45,7 @@ const NoFeedBack = () => {
 	);
 };
 
+//Component to render each statistic
 const Statistics = (props) => {
 	const good = props.good;
 	const neutral = props.neutral;
@@ -66,37 +68,54 @@ const Statistics = (props) => {
 	);
 };
 
+// The main App component
 function App(props) {
+	//Hooks to manage states
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
-	//const [average, setAverage] = useState(0);
 
+	// A function that update the goodCount state
 	const goodCount = () => {
 		setGood(good + 1);
 	};
+
+	//Function to update neutralCount state
 	const neutralCount = () => {
 		setNeutral(neutral + 1);
 	};
+
+	//Function to update badCount state
 	const badCount = () => {
 		setBad(bad + 1);
 	};
 
+	//Calculate the total of  all the counts
 	const all = good + neutral + bad;
 
+	/*The function below accepts good as positive, the 
+	impact of neutral is zero so we simply exclude it but bad is 
+	negative, so negate the variable appropriately. The function 
+	basically returns the impact of each variable  */
 	const calcAverage = (a, b, d) => {
 		let c = -b;
+
 		let ans = (Number(a) + Number(c)) / d;
 		return ans;
 	};
+
+	/*The function below calculate the percentage of responds that 
+	is positive which is good responds by total times 100*/
 	const calcPositive = (a, b) => {
 		let pos = (parseFloat(a) / parseFloat(b)) * 100;
 		return pos;
 	};
 
+	/* we calculate the average by invoking the calAverage function */
 	const average = calcAverage(good, bad, all);
 
-	const positive = `${calcPositive(good, all)}%`;
+	/*we calculate the positive percentage by invoking calcPositive function*/
+	const positivePercentage = `${calcPositive(good, all)}%`;
 
 	return (
 		<div>
@@ -112,7 +131,7 @@ function App(props) {
 				bad={bad}
 				all={all}
 				average={average}
-				positive={positive}
+				positive={positivePercentage}
 			/>
 		</div>
 	);
